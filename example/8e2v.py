36e5915915848148a7071daa8dd44a96f5e8c12f
GlebMukhievPI <136423005+progibator22@users.noreply.github.com>
2024-10-21 20:15:27 +0500

    Create 8e2v.py

---
+++ example/8e2v.py
@@ -0,0 +1,7 @@
+def book_list(books, func):
+    for book in books:
+        print(func(book))
+books = ['System Design','Python и DevOps','Git. Практическое руководство']
+def book_print(book):
+    return book.upper() + ' - прочитано'
+book_list(books, book_print)
