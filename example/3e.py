a96a3e62471d290ea3efe7b930000c9c4cbcf9b2
GlebMukhievPI <136423005+progibator22@users.noreply.github.com>
2024-10-21 20:00:50 +0500

    Create 3e.py

---
+++ example/3e.py
@@ -0,0 +1,9 @@
+def example(color):
+  if color == "grey":
+    return "This is shark"
+  elif color == "purple":
+    return "This is PURPLE MAN!!!"
+  else:
+    return "I don't know"
+what_is_it = example('blue')
+print(what_is_it)
